ca0d009be4eed91e06ebd3418081de434e35a400
Bjarne Lamczak <b.lamczak@gmail.com>
2021-12-05 14:08:32 +0200

    Added basic routing

--- app.js
+++ app.js
@@ -12,3 +12,33 @@ app.use(express.urlencoded({ extended: true })); // for parsing application/x-ww
 app.set("view engine", "ejs");
 
 app.listen(3000);
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home" });
+});
+
+app.use((req, res) => {
+    res.status(404).render("404", { title: "404" });
+  });
+
+app.get("/posts", async (req, res) => {
+  try {
+    console.log("get posts request has arrived");
+    const posts = await pool.query("SELECT * FROM posts");
+    res.json(posts.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+app.get("/posts/:id", async (req, res) => {
+  try {
+    console.log("get a post request has arrived");
+    const posts = await pool.query("SELECT * FROM posts WHERE id = $1", [
+      req.params.id,
+    ]);
+    res.json(posts.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
