3c581213234baf67eb79bbb2fc34ed9aa9912677
Bjarne Lamczak <b.lamczak@gmail.com>
2021-12-05 15:05:01 +0200

    Corrected /posts

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.get("/posts", async (req, res) => {
   try {
     console.log("get posts request has arrived");
     const posts = await pool.query("SELECT * FROM posts");
-    res.json(posts.rows);
+    res.render("posts", { posts: posts.rows });
   } catch (err) {
     console.error(err.message);
   }
