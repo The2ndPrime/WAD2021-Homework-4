e80fb40af1b9b27333a320f625e29c75b975ab0a
Bjarne Lamczak <b.lamczak@gmail.com>
2021-12-05 15:10:30 +0200

    Formatting

--- app.js
+++ app.js
@@ -49,9 +49,7 @@ app.get("/singlepost/:id", async (req, res) => {
     const id = req.params.id;
     console.log(req.params.id);
     console.log("get a single post request has arrived");
-    const posts = await pool.query("SELECT * FROM posts WHERE id = $1", [
-      id,
-    ]);
+    const posts = await pool.query("SELECT * FROM posts WHERE id = $1", [id]);
     res.render("singlepost", { posts: posts.rows[0] });
   } catch (err) {
     console.error(err.message);
